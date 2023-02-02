4122119f05bd1fa7660c0e5d6278e65dabf79d39
Small Ant <abcsmir.ant@gmail.com>
2023-02-02 17:58:57 +0700

    2 февраля.
    Прошли основы, туда-сюда.

---
+++ lesson_1.py
@@ -0,0 +1,35 @@
+# Математики
+"""
+print(2 ** 3) # возведение в степень => 8
+print(9 ** (1/2))  # степень
+print(10 / 3)  # деление
+print(10 % 3)  # деление(остаток)
+print(10 // 3)  # деление(целая часть)
+"""
+"""
+phrase = "abracadabra"
+print(phrase.count("ab"))
+phrase = "Математика да нет сыр хочу"
+print(phrase.replace("нет", "да"))
+l = phrase.split(" ")
+print(l)
+print(len(l))
+"""
+"""
+phrase = "вЕтеР, ВеТЕР, ты могуЧ"
+print(phrase.upper())
+print(phrase.lower())
+print(phrase.capitalize())
+print(phrase.title())
+"""
+# "вап" -> str(string)
+# 5 -> int(integer)
+# 5.3 -> float(floating)
+n = input("Введи число:")
+print(type(n))  # тип
+print(n.isdigit())  # число ли
+print(n.isalpha())  # строка ли
+
+
+
+
